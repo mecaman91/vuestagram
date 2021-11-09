9304a77f63e1ee2c567880b5f287bbb6229911ad
mecaman91 <83256739+mecaman91@users.noreply.github.com>
2021-11-10 00:36:16 +0900

    Add files via upload

---
+++ precache-manifest.57de11edf7f02e7641a5a902a6bb9899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74da76d1a523c516b608",
+    "url": "/css/app.790a0cf4.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "60dce3443e02263ae6e40737244edd3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74da76d1a523c516b608",
+    "url": "/js/app.3f960313.js"
+  },
+  {
+    "revision": "6217dab4f719114ec027",
+    "url": "/js/chunk-vendors.6cea4592.js"
+  },
+  {
+    "revision": "6a1ad00dd7d473f746f3897d0ce8c52e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
